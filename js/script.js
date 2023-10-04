a5442870e6a1b54bfc3fee35e2b38d51d9bf2d66
Kevin Maulana Atmaja <j04719671@gmail.com>
2023-10-04 20:45:32 +0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,68 @@
+// menyiapkan canvas 
+const mycanvas = document.querySelector('#mycanvas');
+
+// atur ukuran canvas 
+mycanvas.width = window.innerWidth;
+mycanvas.height = window.innerHeight;
+
+// tentukan context 
+const c = mycanvas.getContext('2d');
+
+// manipulasi canvas 
+
+// membuat rectangle / kotak
+// c.fillStyle = 'pink';
+// c.strokeStyle = 'black';
+// c.lineWidth = '5';
+
+// c.rect(50, 50, 300, 300);
+// c.fill();
+// c.stroke();
+
+// // membuat circle / lingkaran / arc
+// c.fillStyle = 'lightgreen';
+// c.beginPath();
+// c.arc(550, 200, 150, 0, 2 * Math.PI);
+// c.fill();
+// c.stroke();
+
+// // membuat path (segitiga)
+// c.fillStyle = 'lightblue';
+// c.beginPath();
+// c.moveTo(900, 50);
+// c.lineTo(1050, 350);
+// c.lineTo(750, 350);
+// // c.lineTo(900, 50);
+// c.closePath();
+// c.fill();
+// c.stroke();
+
+// animasi canvas 
+
+let x = 300;
+let y = 200;
+let speedX = 5;
+let speedY = 5;
+let radius = 150;
+
+function draw() {
+    window.requestAnimationFrame(draw);
+    c.clearRect(0,0, innerWidth, innerHeight);
+    console.log('ok');
+    c.fillStyle = 'lightgreen';
+    c.beginPath();
+    c.arc(x, y, radius, 0, 2 * Math.PI);
+    c.fill();
+    c.stroke(); 
+
+    if( x + radius > innerWidth || x - radius < 0 ){
+        speedX = -speedX;
+    }
+    if( y + radius > innerHeight || y - radius < 0 ){
+        speedY = -speedY;
+    }
+    x += speedX;
+    y += speedY;
+}
+
+draw();
